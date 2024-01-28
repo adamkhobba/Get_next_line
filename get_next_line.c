c786027af2970ee9c7d0c33c8af9342fd5f7d042
Adam Khobba <akhobba@e1r12p5.1337.ma>
2024-01-28 18:53:40 +0100

    100% finished

--- get_next_line.c
+++ get_next_line.c
@@ -6,99 +6,110 @@
 /*   By: akhobba <akhobba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 18:39:22 by akhobba           #+#    #+#             */
-/*   Updated: 2024/01/25 11:04:43 by akhobba          ###   ########.fr       */
+/*   Updated: 2024/01/28 18:45:48 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *ft_read(char *arr, int fd)
+char	*ft_strchr(char *s, int c)
 {
-    char    *str;
-    int     sub; 
-  
-    sub = 1;
-    str = malloc(BUFFER_SIZE + 1);
-    if (!str)
-      return (NULL);
-    while (sub && !ft_strchr(arr, '\n'))
-    {
-      sub = read(fd, str, BUFFER_SIZE);
-	    if (sub == -1)
-		  {
-		  	free(arr);
-		  	free(str);
-		  	return (NULL);
-		  }
-      str[sub] = '\0';
-      arr = ft_strjoin(arr, str);
-      if (!arr)
-        return (NULL);
-  }
-    free(str);
-    return (arr);
-}
+	int	i;
 
-size_t ft_count(char *arr)
-{
-  size_t i;
-
-  i = 0; 
-  while (arr  && arr[i] && arr[i] != '\n')
-      i++;
-  return (i);
-}
-
-char *ft_line(char *arr)
-{
-  char *str; 
-  size_t i;
-
-  i = 0;
-  if (!arr)
-    return ((void *)0);
-  if (*arr == '\0')
-    return (NULL);
-  str = malloc(ft_count(arr) + 2);
-  if (!str)
-    return(NULL);
-  while (arr && arr[i] && i < ft_count(arr) + 1)
-  {
-    str[i] = arr[i];
-    i++;
-  }
-  str[i] = '\0';
-      // printf("%p \n", str);
-  return (str);
-}
-
-char *ft_backup(char *arr)
-{
-  char *reserve;
-  
-  if (!arr)
-    return (NULL);
-  if (ft_count(arr) == ft_strlen(arr))
-  {
-    free(arr);
-    return(NULL);
-  }
-  reserve = ft_strdup(arr + (ft_count(arr) + 1));
-  free(arr);
-  return (reserve);
-}
-
-char  *get_next_line(int fd)
-{
-  static char *arr;
-  char *one;
-
-  if (BUFFER_SIZE <= 0 || fd < 0 || BUFFER_SIZE >= 2147483647 || fd > 1025)
+	i = 0;
+	if (!s)
 		return (NULL);
-  arr = ft_read(arr, fd);
-  one = ft_line(arr);
-  arr = ft_backup(arr);
-  if (one && *one == '\0')
-    return (NULL);
-  return (one);
+	while (s[i] != (char)c && s[i] != '\0')
+		i++;
+	if (s[i] == (char)c)
+		return (&s[i]);
+	return (0);
+}
+
+char	*ft_read(char *arr, int fd)
+{
+	char	*str;
+	int		sub;
+
+	sub = 1;
+	str = malloc(BUFFER_SIZE + 1);
+	if (!str)
+		return (NULL);
+	while (sub && !ft_strchr(arr, '\n'))
+	{
+		sub = read(fd, str, BUFFER_SIZE);
+		if (sub == -1)
+		{
+			free(arr);
+			free(str);
+			return (NULL);
+		}
+		str[sub] = '\0';
+		arr = ft_strjoin(arr, str);
+		if (!arr)
+			return (NULL);
+	}
+	free(str);
+	return (arr);
+}
+
+char	*ft_line(char *arr)
+{
+	char	*str;
+	size_t	i;
+	size_t	k;
+
+	i = 0;
+	k = 0;
+	if (!arr)
+		return ((void *)0);
+	if (*arr == '\0')
+		return (NULL);
+	while (arr && arr[k] && arr[k] != '\n')
+		k++;
+	str = malloc(k + 2);
+	if (!str)
+		return (NULL);
+	while (arr && arr[i] && i < k + 1)
+	{
+		str[i] = arr[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_backup(char *arr)
+{
+	char	*reserve;
+	size_t	i;
+
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (arr && arr[i] && arr[i] != '\n')
+		i++;
+	if (i == ft_strlen(arr))
+	{
+		free(arr);
+		return (NULL);
+	}
+	reserve = ft_strdup(arr + (i + 1));
+	free(arr);
+	return (reserve);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*arr;
+	char		*one;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || BUFFER_SIZE >= 2147483647 || fd > 1025)
+		return (NULL);
+	arr = ft_read(arr, fd);
+	one = ft_line(arr);
+	arr = ft_backup(arr);
+	if (one && *one == '\0')
+		return (NULL);
+	return (one);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -3,34 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhobba <akhobba@student.42.fr>            +#+  +:+       +#+        */
+/*   By: akhobba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/26 12:59:21 by akhobba           #+#    #+#             */
-/*   Updated: 2024/01/26 14:44:49 by akhobba          ###   ########.fr       */
+/*   Created: 2024/01/28 18:48:43 by akhobba           #+#    #+#             */
+/*   Updated: 2024/01/28 18:49:56 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
- 
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 5
-#endif
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-
-char    *get_next_line(int fd);
-char    *ft_strjoin(char *s1, char *s2);
-char    *ft_strdup(char *s);
-size_t  ft_strlen(const char *s);
-size_t  ft_strlcpy(char *dst, char *src, size_t size);
-char    *ft_strchr(char *s, int c);
-char    *ft_backup(char *arr);
-char    *ft_substr(char *s, size_t i, size_t len);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+char	*ft_backup(char *arr);
+char	*ft_substr(char *s, size_t i, size_t len);
 #endif
